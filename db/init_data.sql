83a12cddaa76b35e21f056794b1686c9ac865144
muhamed-hassan <mohamed.hassan.work@gmail.com>
2023-03-20 08:11:05 +0200

    add init-data

---
+++ db/init_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO `iban_configs` (`country_code`, `check_digits`, `bank_code`, `sort_code`) VALUES ('GB', '29', 'NWBK', '601613');
+
+INSERT INTO `currency` (`code`) VALUES ('SAR'), ('USD'), ('EUR');
