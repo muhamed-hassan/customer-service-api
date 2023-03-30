76bec5e67b17a7803b4f96d0f1b27932d88a7897
muhamed-hassan <mohamed.hassan.work@gmail.com>
2023-03-30 08:41:15 +0200

    code refactoring

--- src/main/java/com/poc/interfaces/rest/controllers/Validator.java
+++ src/main/java/com/poc/interfaces/rest/controllers/Validator.java
@@ -21,14 +21,8 @@ public class Validator {
 			return "name is required";
 		}
 		name = name.trim();
-		if (name.length() == 0) {
-			return "name is required";
-		}		
-		if (name.length() > 250) {
-			return "name length exceeded 250 characters";
-		}		
-		if (!name.matches("[a-zA-Z]*")) {
-			return "name contains invalid characters and shall contain letters only";
+		if (!name.matches("[a-zA-Z ]{10,250}")) {
+			return "name contains invalid characters and shall contain letters only with maximum of 250";
 		}
 		
 		String nationalId = userInfoCreateModel.getNationalId();
@@ -36,14 +30,8 @@ public class Validator {
 			return "nationalId is required";
 		}
 		nationalId = nationalId.trim();
-		if (nationalId.length() == 0) {
-			return "nationalId is required";
-		}		
-		if (nationalId.length() > 14) {
-			return "nationalId length exceeded 14 characters";
-		}		
-		if (!nationalId.matches("[0-9]*")) {
-			return "nationalId contains invalid characters and shall contain digits only";
+		if (!nationalId.matches("[0-9]{14}")) {
+			return "nationalId contains invalid characters and shall contain 14 digits only";
 		}
 		
 		String dateOfBirthStr = userInfoCreateModel.getDateOfBirth();
@@ -61,11 +49,8 @@ public class Validator {
 			return "cellPhone is required";
 		}
 		cellPhone = cellPhone.trim();
-		if (cellPhone.length() == 0) {
-			return "cellPhone is required";
-		}
 		if (!cellPhone.matches("(07)[0-9]{9}")) {
-			return "cellPhone contains invalid characters and shall be in this form 071234567";
+			return "cellPhone contains invalid characters and shall be in this form 07123456789";
 		}
 		
 		String email = userInfoCreateModel.getEmail();
@@ -73,9 +58,6 @@ public class Validator {
 			return "email is required";
 		}
 		email = email.trim();
-		if (email.length() == 0) {
-			return "email is required";
-		}
 		if (!email.matches("[a-zA-Z0-9_\\.]+(\\@)[a-zA-Z0-9]+(\\.)[a-zA-Z]+")) {
 			return "email format is invalid";
 		}
@@ -85,23 +67,17 @@ public class Validator {
 			return "city is required";
 		}
 		city = city.trim();
-		if (city.length() == 0) {
-			return "city is required";
-		}		
-		if (city.length() > 50) {
-			return "city length exceeded 50 characters";
-		}		
+		if (!city.matches("[a-zA-Z ]{5,50}")) {
+			return "city contains invalid characters and shall contain letters only with maximum of 50";
+		}	
 		
 		String region = userInfoCreateModel.getRegion();
 		if (region == null) {
 			return "region is required";
 		}
 		region = region.trim();
-		if (region.length() == 0) {
-			return "region is required";
-		}		
-		if (region.length() > 50) {
-			return "region length exceeded 50 characters";
+		if (!region.matches("[a-zA-Z ]{5,50}")) {
+			return "region contains invalid characters and shall contain letters only with maximum of 50";
 		}		
 		
 		String buildingNumber = userInfoCreateModel.getBuildingNumber();
@@ -109,26 +85,17 @@ public class Validator {
 			return "buildingNumber is required";
 		}
 		buildingNumber = buildingNumber.trim();
-		if (buildingNumber.length() == 0) {
-			return "buildingNumber is required";
-		}		
-		if (buildingNumber.length() > 50) {
-			return "buildingNumber length exceeded 50 characters";
+		if (!buildingNumber.matches("[a-zA-Z0-9 ]{5,50}")) {
+			return "buildingNumber contains invalid characters and shall contain alphanumeric characters only with maximum of 50";
 		}		
 		
 		String postalCode = userInfoCreateModel.getPostalCode();
 		if (postalCode == null) {
 			return "postalCode is required";
 		}
-		postalCode = postalCode.trim();
-		if (postalCode.length() == 0) {
-			return "postalCode is required";
-		}		
-		if (postalCode.length() > 5) {
-			return "postalCode length exceeded 5 characters";
-		}		
-		if (!postalCode.matches("[0-9]*")) {
-			return "postalCode contains invalid characters and shall contain digits only";
+		postalCode = postalCode.trim();	
+		if (!postalCode.matches("[0-9]{5}")) {
+			return "postalCode contains invalid characters and shall contain digits only with maximum of 5";
 		}
 		
 		return null;
@@ -141,14 +108,8 @@ public class Validator {
 			return "nationalId is required";
 		}
 		nationalId = nationalId.trim();
-		if (nationalId.length() == 0) {
-			return "nationalId is required";
-		}		
-		if (nationalId.length() > 14) {
-			return "nationalId length exceeded 14 characters";
-		}		
-		if (!nationalId.matches("[0-9]*")) {
-			return "nationalId contains invalid characters and shall contain digits only";
+		if (!nationalId.matches("[0-9]{14}")) {
+			return "nationalId contains invalid characters and shall contain 14 digits only";
 		}
 		
 		String cellPhone = userInfoUpdateModel.getCellPhone();
@@ -156,9 +117,6 @@ public class Validator {
 			return "cellPhone is required";
 		}
 		cellPhone = cellPhone.trim();
-		if (cellPhone.length() == 0) {
-			return "cellPhone is required";
-		}
 		if (!cellPhone.matches("(07)[0-9]{9}")) {
 			return "cellPhone contains invalid characters and shall be in this form 071234567";
 		}
@@ -168,9 +126,6 @@ public class Validator {
 			return "email is required";
 		}
 		email = email.trim();
-		if (email.length() == 0) {
-			return "email is required";
-		}
 		if (!email.matches("[a-zA-Z0-9_\\.]+(\\@)[a-zA-Z0-9]+(\\.)[a-zA-Z]+")) {
 			return "email format is invalid";
 		}
@@ -180,11 +135,8 @@ public class Validator {
 			return "city is required";
 		}
 		city = city.trim();
-		if (city.length() == 0) {
-			return "city is required";
-		}		
-		if (city.length() > 50) {
-			return "city length exceeded 50 characters";
+		if (!city.matches("[a-zA-Z ]{5,50}")) {
+			return "city contains invalid characters and shall contain letters only with maximum of 50";
 		}	
 		
 		String region = userInfoUpdateModel.getRegion();
@@ -192,38 +144,26 @@ public class Validator {
 			return "region is required";
 		}
 		region = region.trim();
-		if (region.length() == 0) {
-			return "region is required";
+		if (!region.matches("[a-zA-Z ]{5,50}")) {
+			return "region contains invalid characters and shall contain letters only with maximum of 50";
 		}		
-		if (region.length() > 50) {
-			return "region length exceeded 50 characters";
-		}
 		
 		String buildingNumber = userInfoUpdateModel.getBuildingNumber();
 		if (buildingNumber == null) {
 			return "buildingNumber is required";
 		}
 		buildingNumber = buildingNumber.trim();
-		if (buildingNumber.length() == 0) {
-			return "buildingNumber is required";
-		}		
-		if (buildingNumber.length() > 50) {
-			return "buildingNumber length exceeded 50 characters";
+		if (!buildingNumber.matches("[a-zA-Z0-9 ]{5,50}")) {
+			return "buildingNumber contains invalid characters and shall contain alphanumeric characters only with maximum of 50";
 		}		
 		
 		String postalCode = userInfoUpdateModel.getPostalCode();
 		if (postalCode == null) {
 			return "postalCode is required";
 		}
-		postalCode = postalCode.trim();
-		if (postalCode.length() == 0) {
-			return "postalCode is required";
-		}		
-		if (postalCode.length() > 5) {
-			return "postalCode length exceeded 5 characters";
-		}		
-		if (!postalCode.matches("[0-9]*")) {
-			return "postalCode contains invalid characters and shall contain digits only";
+		postalCode = postalCode.trim();	
+		if (!postalCode.matches("[0-9]{5}")) {
+			return "postalCode contains invalid characters and shall contain digits only with maximum of 5";
 		}
 		
 		return null;
